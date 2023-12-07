9c62680b2cc2009b053061b2b537723e74423850
ashtilawat23 <ashtilawat23@gmail.com>
2023-12-06 21:04:05 -0500

    error fixes

--- src/auth/supabase.js
+++ src/auth/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabase_url=process.env.VITE_SUPABASE_URL;
-const supabase_key = process.env.VITE_SUPABASE_API_KEY;
+const supabase_key = process.env.VITE_SUPABASE_KEY;
 
 const supabase = createClient(supabase_url, supabase_key);
 
